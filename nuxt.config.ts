f9946459e36b4a7279aff112937b5061f1e74d67
jamiefeiss <jamie.feiss@gmail.com>
2025-10-23 17:08:40 +1000

    Upgraded to Prez UI 4.2.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
-    modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/color-mode"],
+    modules: [ "shadcn-nuxt", "@nuxtjs/color-mode"],
     extends: [
         "prez-ui"
     ],
     vite: {
+        plugins: [tailwindcss()],
         optimizeDeps: {
             include: ["@triply/yasgui"]
         },
@@ -14,15 +17,15 @@ export default defineNuxtConfig({
             template: {
                 compilerOptions: {
                     whitespace: 'preserve' // for preserving whitespace in pre tag in slot
-                }
-            }
-        }
+                },
+            },
+        },
     },
     app: {
         head: {
             title: "IDN Catalogue",
             link: [
-                { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap", type: "text/css" }
+                { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap", type: "text/css" },
             ]
         },
     },
@@ -31,4 +34,9 @@ export default defineNuxtConfig({
             autoSubfolderIndex: false,
         },
     },
+    shadcn: {
+        prefix: "",
+        componentDir: "./app/components/ui"
+    },
+
 });
